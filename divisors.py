39edfbb106753644d986b5000c4441270c39ecfd
JaKeoung Koo <jakeoung@users.noreply.github.com>
2025-06-01 11:30:06 +0900

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -3,7 +3,9 @@ number = int(sys.argv[1])
 
 print("a")
 
-for i in range(1, number+1):
-  if number % i == 0:
-    print(i, end="a ")
+def divide(number):
+  for i in range(1, number+1):
+    if number % i == 0:
+      print(i, end="a ")
 
+  return 3
